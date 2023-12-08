12ff790e18629604cf19c4a89ac651a70a43959f
niksonstha <sthanikson10@gmail.com>
2023-12-08 18:39:14 +0545

    add filtering

--- components/studentlist/studentlist.component.js
+++ components/studentlist/studentlist.component.js
@@ -1,20 +1,50 @@
 studentlist.component("studentlist", {
   templateUrl: "components/studentlist/studentlist.template.html",
-  controller: function ($scope) {
+  controller: function ($scope, uiGridConstants) {
+    $scope.highlightFilteredHeader = function (
+      row,
+      rowRenderIndex,
+      col,
+      colRenderIndex
+    ) {
+      if (col.filters[0].term) {
+        return "header-filtered";
+      } else {
+        return "";
+      }
+    };
+
+    $scope.gridOptions = {
+      enableFiltering: true,
+      columnDefs: [
+        {
+          field: "id",
+          headerCellClass: $scope.highlightFilteredHeader,
+          enableFiltering: false,
+        },
+        {
+          field: "name",
+          headerCellClass: $scope.highlightFilteredHeader,
+        },
+        { field: "age", headerCellClass: $scope.highlightFilteredHeader },
+        { field: "grade", headerCellClass: $scope.highlightFilteredHeader },
+        { field: "major", headerCellClass: $scope.highlightFilteredHeader },
+      ],
+      data: [], // Initialize an empty array for data
+    };
+
     $scope.getData = function () {
-      // Use Axios to make a GET request
+      // Use AngularJS $http to make a GET request
       axios
-        .get("../../db/student_list.json")
+        .get("../../db/student_list.json") // Adjust the path accordingly
         .then(function (response) {
-          // Handle successful response
-          console.log("Data:", response.data);
-          $scope.myData = response.data; // Assign the data to $scope or process it as needed
+          $scope.gridOptions.data = response.data;
         })
         .catch(function (error) {
-          // Handle error
           console.error("Error fetching data:", error);
         });
     };
-    $scope.getData();
+
+    $scope.getData(); // Fetch data when controller initializes
   },
 });
