d370f36375cc2980209efa0304ef8bbe154100d5
niksonstha <sthanikson10@gmail.com>
2023-12-14 16:31:13 +0545

    responsive navbar

--- components/navbar/navbar.component.js
+++ components/navbar/navbar.component.js
@@ -1,11 +1,25 @@
 navbar.component("navbar", {
   templateUrl: "components/navbar/navbar.template.html",
-  controller: function ($scope, $rootScope) {
-    $scope.isSmallerNavbar = false;
+  controller: function ($scope, $rootScope, $window) {
+    function checkIfMobile() {
+      return $window.innerWidth <= 768; // Adjust this value based on your design
+    }
+
+    $scope.isSmallerNavbar = checkIfMobile();
+    $scope.showBarsIcon = !$scope.isSmallerNavbar; // Initially set based on view size
+
+    angular.element($window).bind("resize", function () {
+      $scope.$apply(function () {
+        $scope.isSmallerNavbar = checkIfMobile();
+        $scope.showBarsIcon = !$scope.isSmallerNavbar;
+        $scope.showBarsIcon = !$scope.isSmallerNavbar; // Update visibility based on view size
+      });
+    });
 
     $scope.toggleNavbar = function () {
       $rootScope.$broadcast("toggleNavbarEvent");
       $scope.isSmallerNavbar = !$scope.isSmallerNavbar;
+      // Update visibility after toggle
     };
   },
 });
